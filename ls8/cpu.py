ccb9795297fc2bb02bee1ff357006ae2e646926c
Pyrom <56746576+e-pyrom@users.noreply.github.com>
2020-10-08 20:00:14 -0400

    Day 2 and Day 3 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,10 +1,12 @@
 """CPU functionality."""
-
 import sys
-
+SP = 7
 LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
+MUL = 0b10100010
+PUSH = 0b01000101
+POP = 0b01000110
 
 class CPU:
     """Main CPU class."""
@@ -14,11 +16,13 @@ class CPU:
 
         self.ram = [0] * 256
         self.registers = [0] * 8 # R0-R7
+        self.registers[SP] = 0xF4 # stack pointer
         self.pc = 0 # Program Counter, address of the currently-executing instuction
+
         # "Variables" in hardware. Known as "registers".
         # There are a fixed number of registers
         # They have fixed names
-        #  R0, R1, R2, ... , R6, R7
+        # R0, R1, R2, ... , R6, R7
 
     # accepts the address to read and return the value stored there.
     def ram_read(self, address):
@@ -31,23 +35,27 @@ class CPU:
     def load(self):
         """Load a program into memory."""
 
-        address = 0
+        try:
+            address = 0
+            with open(sys.argv[1]) as file:
+                for line in file:
+                    split_file = line.split("#")
+                    value = split_file[0].strip()
+                    if value == "":
+                        continue
 
-        # For now, we've just hardcoded a program:
+                    try:
+                        instruction = int(value, 2)
+                    except ValueError:
+                        print(f"Invalid number '{value}'")
+                        sys.exit(1)
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+                    self.ram[address] = instruction
+                    address += 1
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        except FileNotFoundError:
+            print(f"{sys.argv[0]} {sys.argv[1]} file not found")
+            sys.exit()
 
 
     def alu(self, op, reg_a, reg_b):
@@ -83,16 +91,88 @@ class CPU:
         """Run the CPU."""
         self.running = True
         while self.running:
-            ir = self.ram_read(self.pc)  # Instruction register
+            instruction = self.ram_read(self.pc)  # Instruction register, copy of the currently-executing instruction
+
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
-            if ir == HLT: # HLT
+            if instruction == HLT: # HLT - halt the CPU and exit the emulator.
                 self.running = False
                 self.pc += 1
-            elif ir == PRN:
+
+            elif instruction == PRN:
                 print(self.registers[operand_a])
                 self.pc += 2
-            elif ir == LDI:
+
+            elif instruction == LDI:
                 self.registers[operand_a] = operand_b
                 self.pc += 3
+
+            elif instruction == MUL:
+                reg_a = self.ram_read(self.pc + 1)
+                reg_b = self.ram_read(self.pc + 2)
+                self.registers[reg_a] = self.registers[reg_a] * self.registers[reg_b]
+                self.pc +=3
+
+            elif instruction == PUSH:
+                # Decrement Stack Pointer(SP)
+                self.registers[SP] -= 1
+
+                # Get the reg num to push
+                register_num = self.ram_read(self.pc + 1)
+
+                # Get the value to push
+                value = self.registers[register_num]
+
+                # Copy the value to the SP address
+                top_of_stack_addr = self.registers[SP]
+                self.ram[top_of_stack_addr] = value
+
+                # increment program counter to put program back on track
+                self.pc += 2
+
+            elif instruction == POP:
+
+                # Get reg to pop into
+                register_num = self.ram_read(self.pc + 1)
+
+                # Get the top of stack addr
+                top_of_stack_addr = self.registers[SP]
+
+		        # Get the value at the top of the stack
+                value = self.ram_read(top_of_stack_addr)
+
+		        # Store the value in the register
+                self.registers[register_num] = value
+
+		        # Increment the SP
+                self.registers[SP] += 1
+
+                # increment program counter to put program back on track
+                self.pc += 2
+
+
+
+
+# Glossary:
+# immediate: takes a constant integer value as an argument
+# register: takes a register number as an argument
+# iiiiiiii: 8-bit immediate value
+# 00000rrr: Register number
+# 00000aaa: Register number
+# 00000bbb: Register number
+# Machine code values shown in both binary and hexadecimal.
+
+# PRN
+# PRN register pseudo-instruction
+# Print numeric value stored in the given register.
+# Print to the console the decimal integer value that is stored in the given register.
+# Machine code:
+# 01000111 00000rrr
+
+# LDI
+# LDI register immediate
+# load "immediate", store a value in a register, or "set this register to this value".
+# Set the value of a register to an integer.
+# Machine code:
+# 10000010 00000rrr iiiiiiii
